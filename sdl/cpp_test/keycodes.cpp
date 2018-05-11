8556c6121e51e5925f6c5c3ebd9d78fc07ffec61
edA-qa mort-ora-y <edaqa@eversystems.eu>
2018-05-11 18:18:04 +0200

    more keystroke support

--- sdl/cpp_test/keycodes.cpp
+++ sdl/cpp_test/keycodes.cpp
@@ -11,6 +11,11 @@ int main() {
 	KEY(SDLK_LEFT)
 	KEY(SDLK_SPACE) //TODO: something wrong here
 	KEY(SDLK_PAUSE)
+	KEY(SDLK_RETURN)
+	KEY(SDLK_w)
+	KEY(SDLK_a)
+	KEY(SDLK_s)
+	KEY(SDLK_d)
 	
 	KEY(SDL_PIXELFORMAT_RGBA8888)
 	return 0;
